7562ec29bea791fff666c68b6dc602dab4d3ab5d
codingrecipe1 <codingrecipe1@gmail.com>
2023-09-22 11:30:29 +0900

    01_저장기능 구현

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,8 +1,15 @@
 package com.example.demo.entity;
 
+import com.example.demo.dto.StudentDTO;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Setter(AccessLevel.PRIVATE)
+@Getter
 @Table(name = "student_table")
 public class StudentEntity {
     @Id
@@ -20,4 +27,24 @@ public class StudentEntity {
 
     @Column(length = 50, nullable = false)
     private String studentMajor;
+
+    // DTO -> Entity 변환 메서드
+    public static StudentEntity toSaveEntity(StudentDTO studentDTO) {
+        StudentEntity studentEntity = new StudentEntity();
+        studentEntity.setStudentNumber(studentDTO.getStudentNumber());
+        studentEntity.setStudentName(studentDTO.getStudentName());
+        studentEntity.setStudentMobile(studentDTO.getStudentMobile());
+        studentEntity.setStudentMajor(studentDTO.getStudentMajor());
+        return studentEntity;
+    }
+
+
+
+
 }
+
+
+
+
+
+
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,11 +1,30 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.StudentDTO;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService {
+    private final StudentRepository studentRepository;
+
     public void save(StudentDTO studentDTO) {
         System.out.println("studentDTO = " + studentDTO);
+        // DTO -> Entity 변환을 위한 메서드 호출
+        StudentEntity studentEntity = StudentEntity.toSaveEntity(studentDTO);
+        studentRepository.save(studentEntity);
     }
 }
+
+
+
+
+
+
+
+
+
+
